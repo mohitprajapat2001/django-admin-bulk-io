751ad840cab16b826a5f529ee1b2d8a5d1d69369
Mohit Prajapat <mohit.prajapat@trootech.com>
2025-02-03 17:50:20 +0530

    [build] update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-admin-bulk-io",
-    version="1.0",
+    version="1.1",
     description="This package allows you to import and export data in your Django admin.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
